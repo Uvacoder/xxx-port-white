cb9a257183bc021076e855754176d55e55c84938
tiff <tiffany@tiffanywhite.dev>
2024-01-06 01:17:20 -0500

    Update something

--- pages/components/icons.jsx
+++ pages/components/icons.jsx
@@ -69,7 +69,7 @@ export function ViewsIcon() {
   );
 }
 
-export function ArrowIcon() {
+export default function ArrowIcon() {
   return (
     <svg
       className="my-0"
@@ -81,7 +81,7 @@ export function ArrowIcon() {
     >
       <path
         d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
+        fill="#1d1d27"
       />
     </svg>
   );
